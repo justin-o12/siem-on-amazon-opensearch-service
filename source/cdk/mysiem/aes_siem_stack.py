8513bb91da10c7d5f25617a2e7efe88277bb7a55
Justin Oursler <justin.oursler@sigmadefense.com>
2025-12-23 15:25:24 -0600

    Removing None subnet id.

--- source/cdk/mysiem/aes_siem_stack.py
+++ source/cdk/mysiem/aes_siem_stack.py
@@ -153,11 +153,12 @@ def get_subnet_ids(context):
         sbunet3 = context.node.try_get_context('imported_vpc_subnet3')
 
         if 'subnet_id' in sbunet1:
-            # treating them all the same
+            # Treating them all the same
             subnet_ids = [sbunet1['subnet_id'], sbunet2['subnet_id'],
                           sbunet3['subnet_id']]
         else:
             subnet_ids = [sbunet1, sbunet2, sbunet3]
+            subnet_ids = [subnet_id for subnet_id in subnet_ids if subnet_id is not None]
     print(f"Returning subnet_ids: {subnet_ids}")
     return subnet_ids
 
